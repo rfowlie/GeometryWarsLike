9742d66d8330d6cdb4a171b2588904c71ac8b79b
rfowlie <robertanthonyfowlie@gmail.com>
2020-11-22 23:04:55 -0500

    AEnemyJump
    extended AEnemy as well as setup a secondary move option so when jumping enemy can move differently than normal.

---
+++ Assets/!GeometeryWars/Enemies/!Surface/Scripts/Enemies/AEnemyJump.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GeometeryWars
+{
+    public abstract class AEnemyJump : AEnemy
+    {
+        public EnemyMovement defaultMovement;
+        public EnemyMovement jumpMovement;
+        private bool isJump = false;
+        private float jumpTime = 0f;
+        public float jumpForce = 10f;
+        public float gravity = 10f;
+        public float delayMin = 1f;
+        public float delayMax = 3f;
+        Coroutine c = null;
+
+        private void SetJump()
+        {
+            jumpTime = 0f;
+            isJump = true;
+            currentMovement = jumpMovement;
+        }
+        protected override void Start()
+        {
+            base.Start();            
+        }
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            jumpTime = 0f;
+            c = StartCoroutine(CoroutineEX.RandomDelayFunc(SetJump, delayMin, delayMax));
+        }
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (isJump)
+            {
+                jumpTime += Time.fixedDeltaTime;
+                float jumpPosition = PhysicsEX.Jump(jumpTime, jumpForce, gravity);
+                if (jumpPosition < 0)
+                {
+                    jumpPosition = 0f;
+                    isJump = false;
+                    c = null;
+                    currentMovement = defaultMovement;
+                    c = StartCoroutine(CoroutineEX.RandomDelayFunc(SetJump, delayMin, delayMax));
+                }
+
+                body.localPosition = Vector3.up * jumpPosition;
+            }
+        }
+    }
+}
---
+++ Assets/!GeometeryWars/Enemies/!Surface/Scripts/Other/PhysicsEX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PhysicsEX
+{
+    //y = Vit - 1/2gt^2
+    public static float Jump(float jumpTime, float jumpForce, float gravity)
+    {
+        return (jumpForce * jumpTime) - 0.5f * gravity * (jumpTime * jumpTime);
+    }
+}
+
