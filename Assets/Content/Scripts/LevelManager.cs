fbf153c67f02f37df705e7420d639cce6b7b35d5
rfowlie <robertanthonyfowlie@gmail.com>
2021-01-11 13:55:56 -0800

    Project clean up

---
+++ Assets/Content/Scripts/LevelManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+using Unity.Burst;
+using Unity.Jobs;
+
+
+//control all aspects of in game level
+namespace GeometeryWars
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private TimeManager time;
+        [SerializeField] private PointsManager points;
+        [SerializeField] private SpawnManager spawn;
+        [SerializeField] private PlayerManager player;
+        private EnemyManager enemy;
+
+
+        private bool isActive = false;
+
+        public static event Action<LevelManager> START;
+        public static event Action END;
+        public static event Action GAMEOVER;
+
+        //get the current points for this level
+        public int GetPoints()
+        {
+            return points.points;
+        }
+
+        //Closing steps for when timer is up and level ends
+        public void PrepareLevelOver()
+        {
+            //stop updating units and player
+            isActive = false;
+            GAMEOVER();
+        }
+
+        private void OnEnable()
+        {
+            player.DEATH += PrepareLevelOver;
+        }
+
+        private void Awake()
+        {
+            time = GetComponent<TimeManager>();
+            points = GetComponent<PointsManager>();
+            spawn = GetComponent<SpawnManager>();
+        }
+
+        private void Start()
+        {
+            START(this);
+            player.Setup(UpgradesController.Instance.GetMovementValue(GameController.Instance.GetStateInfo().levelMovementSpeed),
+                         UpgradesController.Instance.GetFireRateValue(GameController.Instance.GetStateInfo().levelFireRate));
+            enemy = new EnemyManager(spawn);
+            isActive = true;
+        }
+
+        private void Update()
+        {
+            //if level is active...
+            if(isActive)
+            {
+                //run timer..check if level finished
+                if (time.AdjustTime(Time.deltaTime))
+                {
+                    isActive = false;
+                    
+                    //level finished
+                    END();
+                }
+
+                //run spawner
+                spawn.Execute(time.GetTimeFromStart());
+
+                //update player
+                player.UpdatePlayer();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(isActive)
+            {
+                //move enemies
+                enemy.Move();
+
+                //move player
+                player.Move();
+            }
+        }
+    }
+
+    
+
+    //run enemy behaviour/movement using JOB system
+    public class EnemyManager
+    {
+        public EnemyManager(SpawnManager s)
+        {
+            spawn = s;
+        }
+
+        private SpawnManager spawn;
+
+        public void Move()
+        {
+            ObjectPool<AEnemy>[] pools = spawn.GetPools();
+            Debug.Log("<color=blue>Move Enemies</color>");
+            for (int i = 0; i < pools.Length; i++)
+            {
+                List<AEnemy> temp = pools[i].GetActiveObjects();
+                for (int j = 0; j < temp.Count; j++)
+                {
+                    temp[j].Move();
+                }
+            }
+        }
+
+        //Try to make above IJOB
+        public struct EnemyMovement : IJobParallelFor
+        {
+            public void Execute(int index)
+            {
+
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Tools/ExtentionScripts/CoroutineEX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class CoroutineEX
+{
+    //pass a func to be executed on a monobehaviour after set delay
+    public static Coroutine Delay(MonoBehaviour inst, Action func, float delay)
+    {
+        return inst.StartCoroutine(CoroutineDelay(func, delay));
+    }
+    private static IEnumerator CoroutineDelay(Action func, float delay)
+    {
+        if(delay < 0f) { delay = 0f; }
+
+        yield return new WaitForSeconds(delay);
+        func();
+    }
+
+    public static Coroutine RandomDelay(MonoBehaviour inst, Action func, float min, float max)
+    {
+        return inst.StartCoroutine(CoroutineRandomDelay(func, min, max));
+    }
+    //execute a func after a random time between two values
+    private static IEnumerator CoroutineRandomDelay(Action func, float min, float max)
+    {
+        yield return new WaitForSecondsRealtime(UnityEngine.Random.Range(min, max));
+        func();
+    }
+
+    //will execute passed method in next frame
+    //*also allows you to tie the coroutine to other monobehaviours, not just self...
+    public static Coroutine OnNextFrame(MonoBehaviour inst, Action func)
+    {
+        return inst.StartCoroutine(CoroutineOnNextFrame(func));
+    }
+    private static IEnumerator CoroutineOnNextFrame(Action func)
+    {
+        //skips current frame
+        yield return null;
+        func();
+    }
+}
